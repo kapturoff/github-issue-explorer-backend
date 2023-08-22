4404134ccdcccc7cc7acb48dc7b86232514fd67c
Vladimir Gergert <pigunderwater@gmail.com>
2023-08-22 14:43:07 +0600

    feat: provide queries from request to Github

--- src/controllers/issues.controller.ts
+++ src/controllers/issues.controller.ts
@@ -2,15 +2,16 @@ import axios from 'axios';
 import { Request, Response } from 'express';
 
 type TIssuesParams = { owner: string; repositoryName: string };
+type TIssuesQueries = { opened: boolean; page: number; per_page: number };
 
 async function listIssues(
-  request: Request<TIssuesParams>,
+  request: Request<TIssuesParams, null, null, TIssuesQueries>,
   response: Response,
 ) {
   const { owner, repositoryName } = request.params;
   const url = `/repos/${owner}/${repositoryName}/issues`;
 
-  const { data } = await axios.get(url);
+  const { data } = await axios.get(url, { params: request.query });
   response.json(data);
 }
 
